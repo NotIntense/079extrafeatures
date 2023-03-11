8467f20f0d86dae7a2559e6422717165d56bba20
Miki_hero <100715076+Mikihero@users.noreply.github.com>
2023-03-11 02:37:49 +0100

    fixed event handlers causing nre in UnregisteringEvents

--- ServerTools/ServerTools.cs
+++ ServerTools/ServerTools.cs
@@ -8,13 +8,13 @@ namespace ServerTools
 {
     public class ServerTools : Plugin<Config>
     {
-        public static ServerTools Instance { get; private set; }
+        public static ServerTools Instance;
         public override string Name => "079extra";
         public override string Prefix => "079ex";
         public override string Author => "NotIntense#1613";
-        public override PluginPriority Priority { get; } = PluginPriority.Medium;
+        public override PluginPriority Priority => PluginPriority.Medium;
 
-        public override Version Version => new Version(4, 0, 0);
+        public override Version Version => new Version(4, 0, 1);
         public override Version RequiredExiledVersion => new Version(6, 0, 0);
 
         public override void OnEnabled()
@@ -32,24 +32,18 @@ namespace ServerTools
         }
 
         public Handlers.SPlayer player;
-        public Handlers.Server server;
 
         public void RegisterEvents()
         {
             player = new Handlers.SPlayer();
-            server = new Handlers.Server();
 
-            Server.WaitingForPlayers += server.OnWaitingForPlayers;
             Scp079.GainingLevel += player.SCPLevel;
         }
 
         public void UnRegisterEvents()
         {
-            server = null;
-            player = null;
-
-            Server.WaitingForPlayers -= server.OnWaitingForPlayers;
             Scp079.GainingLevel -= player.SCPLevel;
-        }
+        
+            player = null;}
     }
 }
